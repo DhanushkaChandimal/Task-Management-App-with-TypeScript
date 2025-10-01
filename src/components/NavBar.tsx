01311228444f9aa73f508a10cab0a354065a10f4
Dhanushka <dhanushkamcr@gmail.com>
2025-10-01 18:57:19 -0500

    Add authorizarion to nav bar links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,8 +4,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const NavBar :React.FC = () => {
+    const { isAuthenticated } = useAuth0();
+
     return (
         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
             <Container>
@@ -13,8 +16,12 @@ const NavBar :React.FC = () => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#dashboard">Dashboard</Nav.Link>
-                        <Nav.Link href="#dashboard">Create Task</Nav.Link>
+                        {isAuthenticated && 
+                            <>
+                                <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                                <Nav.Link href="/create-task">Create Task</Nav.Link>
+                            </>
+                        }
                     </Nav>
                     <Nav>
                         <LoginButton/>
