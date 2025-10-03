617931a703d3cd34cdf04b5586128504422d8fff
Dhanushka <dhanushkamcr@gmail.com>
2025-10-02 19:10:13 -0500

    Added authorization to Dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./components/HomePage"
 import Dashboard from "./components/Dashboard"
+import AuthenticationGuard from "./components/AuthenticationGuard"
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage/>}></Route>
-      <Route path="/dashboard" element={<Dashboard/>}></Route>
+      <Route path="/dashboard" element={<AuthenticationGuard component={Dashboard} />}></Route>        
     </Routes>
   )
 }
