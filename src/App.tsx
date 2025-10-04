5b9ad375babcd23f0d6e2079d61cf2a87ebfb88c
Dhanushka <dhanushkamcr@gmail.com>
2025-10-03 22:03:21 -0500

    Add CreateTask component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom"
 import HomePage from "./components/HomePage"
 import Dashboard from "./components/Dashboard"
 import AuthenticationGuard from "./components/AuthenticationGuard"
+import CreateTask from "./components/CreateTask"
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage/>}></Route>
-      <Route path="/dashboard" element={<AuthenticationGuard component={Dashboard} />}></Route>        
+      <Route path="/dashboard" element={<AuthenticationGuard component={Dashboard} />}></Route>
+      <Route path="/create-task" element={<AuthenticationGuard component={CreateTask} />}></Route>
     </Routes>
   )
 }
---
+++ src/components/CreateTask.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import PageLayout from "./PageLayout";
+
+const CreateTask : React.FC = () => {
+    return (
+        <PageLayout>
+            <h1>Create Task</h1>
+        </PageLayout>
+    );
+};
+
+export default CreateTask;
