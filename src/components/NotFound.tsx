91fed6ed33e8dc1e3d82f24de78b648065047f2e
Dhanushka <dhanushkamcr@gmail.com>
2025-10-10 18:21:20 -0500

    Add NotFound component

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,11 @@
+import PageLayout from "./PageLayout";
+
+const NotFound = () => {
+    return (
+        <PageLayout>
+            <h1>Page Not Found</h1>
+        </PageLayout>
+    );
+};
+
+export default NotFound;
